def87114be1f5fe75ad983c0c1863d04435e4ff8
Abdullah Shakib <abdullahshakib13@gmail.com>
2023-09-14 22:29:21 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="text-3xl font-bold text-center p-4">
+            <h2>Course Registration</h2>
+        </div>
+    );
+};
+
+export default Header;
